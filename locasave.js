2f562657fa4ed8ddeacca4e5aee3c85ef0ecd433
mrdanhdanh <nguyenhuudanh9999@gmail.com>
2021-12-13 17:21:24 +0700

    a

---
+++ locasave.js
@@ -0,0 +1,41 @@
+const DATA = [{
+        date: "2021-12-12",
+        time_in: "08:00",
+        time_out: "16:00",
+        location: "Nhà Moon",
+        geo_location: "https://www.google.com/maps/place/7B+Military+Medical+Hospital/@10.9657838,106.8503866,17z/data=!3m1!4b1!4m5!3m4!1s0x3174dc23eaaaaaab:0x799426b45ce467e6!8m2!3d10.9657785!4d106.8525806"
+    }, 
+    {
+        date: "2021-12-12",
+        time_in: "17:30",
+        time_out: "18:00",
+        location: "Ministop",
+        geo_location: "https://www.google.com/maps/place/MINISTOP/@10.790843,106.6614953,19.08z/data=!4m12!1m6!3m5!1s0x31752932d4caaa59:0x25aea94eb7fc902f!2sMINISTOP!8m2!3d10.7912556!4d106.6618081!3m4!1s0x31752932d4caaa59:0x25aea94eb7fc902f!8m2!3d10.7912556!4d106.6618081"
+    },
+    {
+        date: "2021-12-12",
+        time_in: "18:00",
+        time_out: "",
+        location: "Home",
+        geo_location: "https://www.google.com/maps/place/1036%2F52%2F2+Cách+Mạng+Tháng+Tám,+Phường+5,+Tân+Bình,+Thành+phố+Hồ+Chí+Minh+700000,+Vietnam/@10.7923025,106.6587833,19.58z/data=!4m9!1m2!2m1!1zMTAzNi81MiBDw6FjaCBN4bqhbmcgVGjDoW5nIDgsIFTDom4gQsOsbmgsIEhvIENoaSBNaW5oIENpdHk!3m5!1s0x317529ece62d4bb9:0xb47e52503f815d06!8m2!3d10.7923752!4d106.6588428!15sCjsxMDM2LzUyIEPDoWNoIE3huqFuZyBUaMOhbmcgOCwgVMOibiBCw6xuaCwgSG8gQ2hpIE1pbmggQ2l0eZIBEGdlb2NvZGVkX2FkZHJlc3M"
+    },
+    {
+        date: "2021-12-13",
+        time_in: "12:30",
+        time_out: "12:40",
+        location: "ATM ACB",
+        geo_location: "https://www.google.com/maps/place/Ngân+Hàng+ACB/@10.7970819,106.6648328,19z/data=!4m5!3m4!1s0x3175292de3b0c435:0x90da4149f3e81e8a!8m2!3d10.7970143!4d106.6648448"
+    },
+    {
+        date: "2021-12-13",
+        time_in: "12:40",
+        time_out: "",
+        location: "Cafe G7",
+        geo_location: "https://www.google.com/maps/place//@10.792671,106.6677869,18.96z/data=!4m5!3m4!1s0x317529af90139ec3:0x6d015b82478ee43b!8m2!3d10.7933759!4d106.6686359"
+    }
+
+];
+
+function getData() {
+return DATA;
+}
