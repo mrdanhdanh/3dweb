0201ef19e796417f4ab01b8dda5ec12fe8a3a133
mrdanhdanh <nguyenhuudanh9999@gmail.com>
2021-12-16 14:36:47 +0700

    Update locasave.js

--- locasave.js
+++ locasave.js
@@ -29,6 +29,7 @@ const DATA = [{
     {
         date: "2021-12-13",
         time_in: "12:40",
+<<<<<<< Updated upstream
         time_out: "20:30",
         location: "Cafe G7",
         geo_location: "https://www.google.com/maps/place//@10.792671,106.6677869,18.96z/data=!4m5!3m4!1s0x317529af90139ec3:0x6d015b82478ee43b!8m2!3d10.7933759!4d106.6686359"
@@ -40,6 +41,26 @@ const DATA = [{
         location: "Rửa xe",
         geo_location: "https://www.google.com/maps/place/2+Hẻm+192+Nguyễn+Thái+Bình,+Phường+12,+Tân+Bình,+Thành+phố+Hồ+Chí+Minh,+Vietnam/@10.7966891,106.6537361,20.67z/data=!4m5!3m4!1s0x31752935e4ed8baf:0xacac74047a568ef!8m2!3d10.7967415!4d106.6536401"
         }
+=======
+        time_out: "20:00",
+        location: "G-Cafe",
+        geo_location: "https://www.google.com/maps/place//@10.792671,106.6677869,18.96z/data=!4m5!3m4!1s0x317529af90139ec3:0x6d015b82478ee43b!8m2!3d10.7933759!4d106.6686359"
+    },
+    {
+        date: "2021-12-13",
+        time_in: "20:15",
+        time_out: "20:30",
+        location: "Ministop",
+        geo_location: "https://www.google.com/maps/place/MINISTOP/@10.790843,106.6614953,19.08z/data=!4m12!1m6!3m5!1s0x31752932d4caaa59:0x25aea94eb7fc902f!2sMINISTOP!8m2!3d10.7912556!4d106.6618081!3m4!1s0x31752932d4caaa59:0x25aea94eb7fc902f!8m2!3d10.7912556!4d106.6618081"
+    },
+    {
+        date: "2021-12-13",
+        time_in: "20:45",
+        time_out: "",
+        location: "Home",
+        geo_location: "https://www.google.com/maps/place/1036%2F52%2F2+Cách+Mạng+Tháng+Tám,+Phường+5,+Tân+Bình,+Thành+phố+Hồ+Chí+Minh+700000,+Vietnam/@10.7923025,106.6587833,19.58z/data=!4m9!1m2!2m1!1zMTAzNi81MiBDw6FjaCBN4bqhbmcgVGjDoW5nIDgsIFTDom4gQsOsbmgsIEhvIENoaSBNaW5oIENpdHk!3m5!1s0x317529ece62d4bb9:0xb47e52503f815d06!8m2!3d10.7923752!4d106.6588428!15sCjsxMDM2LzUyIEPDoWNoIE3huqFuZyBUaMOhbmcgOCwgVMOibiBCw6xuaCwgSG8gQ2hpIE1pbmggQ2l0eZIBEGdlb2NvZGVkX2FkZHJlc3M"
+    }
+>>>>>>> Stashed changes
 
 ];
 
